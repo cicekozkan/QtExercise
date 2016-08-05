addf7327eecbf6109749608a345ad01bab1e64c3
ocicek <ozkan.cicek@analog.com>
2016-08-05 15:37:13 +0300

    QDir::currentPath() static member function returns the current working directory of the executable. setCurrent() static member functions changes it

--- 020-QDir/main.cpp
+++ 020-QDir/main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  QDir dir("C:\\windows");
+  cout << QDir::currentPath().toStdString() << endl;
+
+  QDir::setCurrent("C:\\windows");
+
+  QDir dir(".");
 
   for(QFileInfo qf : dir.entryInfoList(QDir::Files | QDir::Executable))
     cout << qf.fileName().toStdString() << ", " << qf.size() << endl;
