cd81b7f8e57b0625172ed31f34793a824b5b7ec5
ocicek <ozkan.cicek@analog.com>
2016-08-05 15:11:04 +0300

    QDir::entryList lists directories

--- 020-QDir/main.cpp
+++ 020-QDir/main.cpp
@@ -2,25 +2,19 @@
 #include <QDir>
 #include <QDebug>
 #include <iostream>
+#include <QStringList>
+#include <QString>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
   QDir dir("C:\\Data\\QtExercise");
-  qDebug() << (dir.exists() ? "Var": "Yok");
-  qDebug() << "dirName = " << dir.dirName();
-  qDebug() << "path = " << dir.path();
 
-  if(!dir.mkdir("test"))
-    cerr << "cannot create test directory!...\n";
-  else
-    cout << "test directory created\n";
+  QStringList sl = dir.entryList(QDir::Files | QDir::Executable | QDir::Hidden);
 
-  if(!dir.rmdir("test"))
-    cerr << "cannot delete test directory!...\n";
-  else
-    cout << "test directory deleted\n";
+  for(QString name : sl)
+    qDebug() << name;
 
   return 0;
 }
