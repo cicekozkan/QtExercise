4ef666d46cf14723faf4d113ae3639a04bab2e7b
ocicek <ozkan.cicek@analog.com>
2016-08-09 11:43:23 +0300

    you can overload << operator for QTextStream and your own class

--- 021-QFile/main.cpp
+++ 021-QFile/main.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <QFile>
-#include <QDataStream>
+#include <QTextStream>
 
 using namespace std;
 
+class Sample{
+private:
+  int m_a, m_b;
+public:
+  Sample(int a, int b):m_a{a}, m_b{b}{}
+  int a(void) const {return m_a;}
+  int b(void) const {return m_b;}
+};
+
+QTextStream& operator<< (QTextStream &ts, const Sample &s)
+{
+  ts << s.a() << ", " << s.b() << endl;
+  return ts;
+}
+
 int main(int argc, char *argv[])
 {
-  QFile file("test.dat");
+  QFile file("test.txt");
 
-  if(!file.open(QIODevice::ReadOnly)){
+  if(!file.open(QIODevice::WriteOnly | QIODevice::Truncate)){
     cerr << "cannot open file!..\n";
     exit(EXIT_FAILURE);
   }//end if file open
 
-  QDataStream ds(&file);
-  int i;
+  QTextStream ts(&file);
+  Sample s(10, 20);
 
-  for(;;){
-    ds >> i;
-    cout << i << endl;
-    if(ds.atEnd())
-      break;
-  }
+  ts << s;
 
   file.close();
 
