66febb0b036baf13204820e4ac7ec458d2846787
ocicek <ozkan.cicek@analog.com>
2016-08-09 10:16:08 +0300

    static member function rename

--- 021-QFile/main.cpp
+++ 021-QFile/main.cpp
@@ -7,17 +7,12 @@ int main(int argc, char *argv[])
 {
   QFile file("a.dat");
 
-  if(!file.open(QIODevice::WriteOnly)){
-    cerr << "cannot open file!..\n";
+  if(!QFile::rename("a.dat", "b.dat")){
+    cerr << "cannot rename file!..\n";
     exit(EXIT_FAILURE);
   }//end if file open
 
-  cout << "File exists?: " << (file.exists()?"Yes":"No") << endl;
-  cout << "Is file open?: " << (file.isOpen()?"Yes":"No") << endl;
-
-  cout << "OK!\n";
-
-  file.close();
+  cout << "File renamed!\n";
 
   return 0;
 }
