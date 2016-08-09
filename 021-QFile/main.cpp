c3d536bb35823564b064dae592acc6ab6efe3c44
ocicek <ozkan.cicek@analog.com>
2016-08-09 10:30:09 +0300

    QIODevice::write(const char *data, qint64 maxSize)

--- 021-QFile/main.cpp
+++ 021-QFile/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <QFile>
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,14 +8,22 @@ int main(int argc, char *argv[])
 {
   QFile file("test.txt");
 
-  if(!file.open(QIODevice::ReadOnly)){
+  if(!file.open(QIODevice::ReadWrite | QIODevice::Truncate)){
     cerr << "cannot open file!..\n";
     exit(EXIT_FAILURE);
   }//end if file open
 
-  QByteArray qba = file.read(100);
+  for(int i = 0; i < 100; i++)
+    file.write(reinterpret_cast<const char*>(&i), sizeof(int));
 
-  cout << qba.toStdString() << endl;
+  file.reset();
+
+  int val;
+  for(int i = 0; i < 100; i++){
+    file.read(reinterpret_cast<char *>(&val), sizeof(int));
+    cout << val << " ";
+  }
+  cout << endl;
 
   file.close();
 
