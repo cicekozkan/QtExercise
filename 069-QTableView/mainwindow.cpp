ff6ab6081c7cf8344de5dc6cf859377c87d1c854
cicekozkan <cicekozkan@gmail.com>
2016-11-13 22:44:01 +0300

    insertRow method inserts a row to the given position

--- 069-QTableView/mainwindow.cpp
+++ 069-QTableView/mainwindow.cpp
@@ -16,6 +16,10 @@ MainWindow::MainWindow(QWidget *parent) :
     m_im->setHeaderData(1, Qt::Horizontal, "No");
     m_im->setHeaderData(2, Qt::Horizontal, "Doğum Tarihi");
     ui->m_tableView->setModel(m_im);
+    m_im->insertRow(0);
+    m_im->setData(m_im->index(0, 0), "Ozkan Cicek");
+    m_im->setData(m_im->index(0, 1), "1080");
+    m_im->setData(m_im->index(0, 2), "06/11/1987");
 
     QFont font("Times New Roman", 12);
     font.setBold(true);
